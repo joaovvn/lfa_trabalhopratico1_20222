01dd22de17b95f1e459b2bb2c53fe441e7515667
João Victor Nogueira <78765569+joaovvn@users.noreply.github.com>
2022-09-19 09:36:34 -0300

    Correção do funcionamento

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
-const { getAutomatosInformation } = require("./utils");
+const { getAutomatosInformation, validateAutomato } = require("./utils");
 
 const readFile = () => {
-  return fs.readFileSync("./example.txt").toString().split("\n");
+  return fs.readFileSync("./abb-baa.txt").toString().split("\n");
 };
 
 (async () => {
   const file = readFile();
   const formatedAutomato = getAutomatosInformation(file);
-  console.log("file", file);
-  console.log("formatedAutomato", formatedAutomato);
+  // 1001001
+  // 0011
+  // 0000000
+  const response = validateAutomato("aaabb", formatedAutomato);
+  console.log("[RESPONSE]:", response);
 })();
